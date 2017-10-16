25cb341c86a076de91d1706ddb1087d3236cf3e4
doudoulaiye <506690263@qq.com>
2017-10-17 00:00:33 +0800

    修改时间选择
    时间滚动和点击事件

---
+++ domevent.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.basetime').hover(function(event) {
+        $(event.target).addClass("btnbg");
+    }, function() {
+        console.log(currentTime!=event.target.innerHTML,currentTime,event.target.innerHTML);
+        if(currentTime!=event.target.innerHTML){
+            $(event.target).removeClass("btnbg");
+        }
+    });
+    $('.basetime').click(function(event) {
+        $('.basetime').removeClass("btnbg");
+        currentTime = event.target.innerHTML;
+        $(event.target).addClass("btnbg");
+    });
+ })
