492095cb518d580eed788a71bb5438bda10dbfd0
doudoulaiye <506690263@qq.com>
2017-10-18 00:03:21 +0800

    轨迹线
    添加事件线的绘制

--- domevent.js
+++ domevent.js
@@ -13,4 +13,32 @@ $(document).ready(function(){
         $(event.target).addClass("btnbg");
     });
  });
+/**
+ * 绘制事件线
+ *
+ */
+function eventline(events){
+    //let date
+    debugger
+    let material = new THREE.LineBasicMaterial({ color: 0x00ffff, opacity: 0.1, linewidth: 1 });
+    var tmp_geo = new THREE.Geometry();
+    for(let i=0;i<events.length;i++){
+        let event = events[i];
+        let xy = event.position.split(" ");
+        let x = xy[0]-0;
+        let y = xy[1]-0;
 
+        let webxy = LonLat2WebGL(x,y);
+        tmp_geo.vertices.push(new THREE.Vector3(webxy.x,webxy.y, mapZValue-1));
+    }
+
+    let line = new THREE.Line(tmp_geo,material);
+    //line.scale.x = line.scale.y = line.scale.z = 1;
+    //line.originalScale = 1;
+
+    // NOTE: Deactivated frustumCulled, otherwise it will not draw all lines (even though
+    // it looks like the lines are in the view frustum).
+    //line.frustumCulled = false;
+
+    rightGroup.add(line);
+}
